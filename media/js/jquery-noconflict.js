77253d824cb964a352bae8ef7fc9b046a82e725a
Llewellyn van der Merwe <llewellyn@vdm.io>
2014-04-01 02:07:24 +0200

    Start of version 2

--- media/js/jquery-noconflict.js
+++ media/js/jquery-noconflict.js
@@ -1,9 +1,9 @@
 /**
 * 
-* 	@version 	1.0.0 July 24, 2013
-* 	@package 	Cost Benefit Projection Tool Application
+* 	@version 	2.0.0 March 13, 2014
+* 	@package 	Staff Health Cost Benefit Projection
 * 	@author  	Vast Development Method <http://www.vdm.io>
-* 	@copyright	Copyright (C) 2013 German Development Cooperation <http://www.giz.de>
+* 	@copyright	Copyright (C) 2014 German Development Cooperation <http://www.giz.de>
 * 	@license	GNU General Public License <http://www.gnu.org/copyleft/gpl.html>
 *
 **/
